243a4d2d0055ad3283cdde3b9e063d8392b1a161
darshilpanchal22 <panchaldarshil145@gmail.com>
2025-09-16 16:58:57 +0530

    .

---
+++ Javascript/functions/index.js
@@ -0,0 +1,21 @@
+// function greet() {
+//     console.log("hello peoples")
+// }
+// greet();
+
+//function
+//? a function is a set of instructions to perform a specific task is called  as function
+//? syntax
+function identifier() {
+
+}
+identifier();
+
+//! function call statement 
+
+
+function greet() {
+    console.log("heloo  everyone")
+}
+greet();
+
---
+++ Javascript/looping/index.js
@@ -0,0 +1,33 @@
+//! looping
+//! for-loop
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+let arr = [
+    "prabhas",
+    "prabhas",
+    "prabhas",
+    "prabhas",
+    "prabhas",
+    "prabhas",
+    "prabhas"
+]
+
+console.log(arr)
+
+// while loop
+let num = 20;
+while (num > 0) {
+    if (num % 2 === 0) {
+        console.log(num);
+    }
+    num--;
+}
+
+// do while loop
+let j = 1;
+do {
+    console.log(j);
+    j++;
+} while (j <= 10);
+
