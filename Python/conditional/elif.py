88c216a82cc54cc94ef85edefdc9fe99f958db66
darshilpanchal22 <panchaldarshil145@gmail.com>
2025-09-16 17:40:23 +0530

    .

---
+++ Python/conditional/elif.py
@@ -0,0 +1,8 @@
+i=2
+if i ==2:
+    print('this is  executed by if')
+
+elif i>2:
+    print('this is excuted bt elif')
+else:
+    print('it is  executed  by else block')
---
+++ Python/conditional/elif1.py
@@ -0,0 +1,22 @@
+# i=(int(input("enter the number")))
+# if 0<=i<=9:
+#     print("single digit")
+# elif 10<=i<=99:
+#     print("double digit")
+# elif 100<=i<=999:
+#     print("three digits")
+# else:
+#     print("it is  greater than 3 digits")
+
+
+#cheack result and define result grade
+per=(int(input("enter the result")))
+if per >= 75.00:
+    print(f"your per is : {per} grade-a")
+elif per >= 60.00:
+    print(f"your per is : {per} grade-b")
+elif per <= 60.00:
+    print(f"your per is: {per} grade-c")   
+
+
+    #to check  the uppercase char , lowercase, digits, special char,
