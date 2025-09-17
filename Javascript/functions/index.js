cb97fb68ee504127224891424cec97667ccd4050
darshilpanchal22 <panchaldarshil145@gmail.com>
2025-09-17 12:27:24 +0530

    .

--- Javascript/functions/index.js
+++ Javascript/functions/index.js
@@ -1,21 +1,70 @@
-// function greet() {
-//     console.log("hello peoples")
-// }
-// greet();
+console.log("Function in JS");
 
-//function
-//? a function is a set of instructions to perform a specific task is called  as function
-//? syntax
+//! Function:
+//? A function is a set of instructions to perform a speicifc task is called as function.
+
+//? Syntax:
 function identifier() {
-
+    //code
 }
-identifier();
 
-//! function call statement 
+identifier(); // Function Call statement
 
+//! Function Call Statement: A statement which is reponsible to call or invoke the function
+//!                         is called as function call statement.
+
+//? Syntax: identifier()
 
 function greet() {
-    console.log("heloo  everyone")
+    console.log("Hello, Everyone!");
 }
+
+// call or invoke the function greet
 greet();
 
+//! Types of Function:
+//? 1. Named Function
+//? 2. Anonymous Function
+//? 3. Function as an Expression
+//? 4. IIFE
+//? 5. Arrow Function
+//? 6. Callback Function
+//? 7. HOF
+//? 8. Nested Function
+//? 9. Closure Function
+//? 10. Function Currying
+//? 11. Generator Function
+
+//! 1. Named Function:
+//? A function which has a name is called as Named Function.
+function getUsername(username) {
+    console.log(`My username is ${username}!`);
+}
+
+// calling or invoking the function
+getUsername("Rohit");
+
+//! 2. Anonymous Function:
+//? A function which don't have any name is called as Anonymous Function.
+
+//? Ways to call the anonymous function:
+//~ 1. By storing it inside one variable and call that variable.
+let fun = function () {
+    console.log("this is anonymous function");
+};
+fun();
+
+//~ 2. By Wrapping entire function inside the () and call inside with empty ();
+
+(function () {
+    console.log("This function is invoked by using empty ()");
+})();
+
+//! 3. Function as an Expression:
+//? Storing a entire function inside one variable is called as function as an expression.
+
+let demo = function () {
+    console.log("This is function as an expression");
+};
+
+demo();
