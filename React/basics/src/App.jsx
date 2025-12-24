0675f122826eae2a15290cbb77ad048132766bdf
darshilpanchal22 <panchaldarshil145@gmail.com>
2025-12-24 20:44:48 +0530

    .

--- React/basics/src/App.jsx
+++ React/basics/src/App.jsx
@@ -3,11 +3,20 @@ import Card from "./components/Card";
 import Projectwithprops from "./components/Projectwithprops";
 
 const App = () => {
+  const arr = [10, 20, 30, 40];
+
   return (
     <>
+      <div>
+        {arr.map(function (elem, index) {
+          return <p key={index}>{elem}</p>;
+        })}
+      </div>
+
       {/* <Card /> */}
       {/* <Card className="border border-black mt-10" /> */}
-      <Projectwithprops />
+      {/* <Projectwithprops /> */}
+      {/* <User /> */}
     </>
   );
 };
