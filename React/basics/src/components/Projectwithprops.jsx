6ff818307a1c5ac91dddfba0dfd9b6c0f0175aa2
darshilpanchal22 <panchaldarshil145@gmail.com>
2025-12-25 15:47:53 +0530

    .

--- React/basics/src/components/Projectwithprops.jsx
+++ React/basics/src/components/Projectwithprops.jsx
@@ -1,44 +1,31 @@
-import React from "react";
-import { Bookmark } from "lucide-react";
+import { Bookmark } from 'lucide-react'
+const Card = (props) => {
 
-const Projectwithprops = () => {
   return (
-    <div className="parent">
-      <div className="card">
-        {/* Top section */}
+    <div className="card">
+      <div>
         <div className="top">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/d/de/Amazon_icon.png"
-            alt="Amazon logo"
-          />
-
-          <button>
-            Save
-            <Bookmark size={23} />
-          </button>
+          <img src={props.brandLogo} alt="nachioooooo" />
+          <button>Save <Bookmark size={14} /> </button>
         </div>
-        {/* Center section */}
         <div className="center">
-          <h3>
-            Amazon <span>5 days ago</span>
-          </h3>
-
-          <h2>Senior UI/UX Designer</h2>
-
-          <h4>Part-time Senior-level</h4>
-        </div>
-        {/* Bottom section */}
-        <div className="bottom">
-          <div>
-            <h3>$120/hour</h3>
-            <p>Ahmedabad - Gujarat</p>
+          <h3>{props.company} <span>{props.datePosted}</span></h3>
+          <h2>{props.post}</h2>
+          <div className='tag'>
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
           </div>
-
-          <button>Apply now</button>
         </div>
       </div>
+      <div className="bottom">
+        <div>
+          <h3>{props.pay}</h3>
+          <p>Mumbai, India</p>
+        </div>
+        <button>Apply Now</button>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Projectwithprops;
+export default Card
