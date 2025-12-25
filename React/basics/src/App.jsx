88d6f11c57083f9cfa48097ce3b75b30b94ea412
darshilpanchal22 <panchaldarshil145@gmail.com>
2025-12-25 16:02:17 +0530

    .

--- React/basics/src/App.jsx
+++ React/basics/src/App.jsx
@@ -111,7 +111,7 @@ const App = () => {
       {/* <Carddesign /> */}
       {jobOpenings.map(function (elem, idx) {
         return <div key={idx}>
-          <Card company={elem.companyName} datePosted={elem.datePosted} post={elem.post} tag1={elem.tag1} brandLogo={elem.brandLogo} pay={elem.pay} tag2={elem.tag2} />
+          <Card company={elem.companyName} datePosted={elem.datePosted} post={elem.post} tag1={elem.tag1} brandLogo={elem.brandLogo} pay={elem.pay} tag2={elem.tag2} location={elem.location} />
         </div>
       })}
     </div>
