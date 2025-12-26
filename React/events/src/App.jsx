62307a99635efa2096eeab3d599f3e4411f95c60
darshilpanchal22 <panchaldarshil145@gmail.com>
2025-12-26 13:36:37 +0530

    .

--- React/events/src/App.jsx
+++ React/events/src/App.jsx
@@ -1,27 +1,37 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [value, setName] = useState("darshil");
+  // const [count, setCount] = useState(0);
+  const [name, setName] = useState("darshil");
 
   const handleClick = () => {
     alert("hey im clicked");
   };
+
   const handleMouseOver = () => {
     alert("hey i am mouse over");
   };
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <>
       <div className="button">
         <button onClick={handleClick}>click me</button>
       </div>
+
       <div className="red" onMouseOver={handleMouseOver}>
-        im red dev
+        im red div
       </div>
-      <input type="text" value={name} />
+
+      <input
+        type="text"
+        value={name}
+        onChange={handleChange}
+      />
     </>
   );
 }
