0586890952395ba15149ee38bdf0ed4f73eefaa7
darshilpanchal22 <panchaldarshil145@gmail.com>
2026-01-03 15:42:23 +0530

    .

--- React/Hooks/usestate/src/components/Usestate1.jsx
+++ React/Hooks/usestate/src/components/Usestate1.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { useState } from 'react'
+
+
+// const Usestate1 = () => {
+//     const [num, Setnum] = useState({ user: 'sarthak', age: 20 })
+//     const btnclicked = () => {
+//         const newNum = { ...num }
+//         newNum.user = "aman"
+//         newNum.age = "29"
+//         Setnum(newNum)
+//     }
+//     return (
+//         <div>
+//             <h1>{num.user}, {num.age}</h1>
+//             <button onClick={btnclicked}>click</button>
+//         </div>
+//     )
+// }
+
+
 
 const Usestate1 = () => {
+    const [num, Setnum] = useState([10, 20, 30, 40])
+    const btnclick = () => {
+        const newNum = [...num]
+        newNum.push(99)
+        // console.log(newNum)
+        Setnum(newNum)
+    }
     return (
-        <div>Usestate1</div>
+        <div>
+            <h1>{num}</h1>
+            <button onClick={btnclick}>click</button>
+        </div>
     )
 }
+
 
 export default Usestate1
