aab78a9055095c000235d1bf583611ff8e51e3da
darshilpanchal22 <panchaldarshil145@gmail.com>
2026-01-08 11:03:35 +0530

    .

--- Python/tasks/10.py
+++ Python/tasks/10.py
@@ -1,10 +1,23 @@
-def longest_word(a):
-    s=a.split()
-    max=len(s[0])
-    word=''
-    for i in s:
-        if len(i)>max:
-            max=len(i)
-            word=i
-    return word
-print(longest_word('python is easy tolearn'))
+# def longest_word(a):
+#     s=a.split()
+#     max=len(s[0])
+#     word=''
+#     for i in s:
+#         if len(i)>max:
+#             max=len(i)
+#             word=i
+#     return word
+# print(longest_word('python is easy tolearn'))
+
+
+#check num is prime or not
+def is_prime(n):
+    for i in range(2, int(n)):
+        if n % i == 0:
+            return False
+    return True
+
+# print(is_prime(11))  # True
+# print(is_prime(15))  # False
+
+#
