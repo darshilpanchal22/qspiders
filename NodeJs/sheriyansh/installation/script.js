e6de60c70f8dc7ce836df1c563ce73940a259b57
darshilpanchal22 <panchaldarshil145@gmail.com>
2026-01-09 13:02:46 +0530

    .

--- NodeJs/sheriyansh/installation/script.js
+++ NodeJs/sheriyansh/installation/script.js
@@ -1,1 +1,32 @@
 const fs = require('fs')
+
+
+//^ for write something and create file
+// fs.writeFile("abcd.txt", "hey heloo", function (err) {
+//     if (err) console.log(err);
+//     else console.log("file created")
+// })
+
+//& for read file
+// fs.readFile("abcd.txt", "utf8", function (err, data) {
+//     if (err) console.log(err)
+//     else console.log(data)
+// })
+
+//* for add data
+// fs.appendFile("abcd.txt", " appended data", function (err) {
+//     if (err) console.log(err)
+//     else console.log("appended data")
+// })
+
+//~ for rename file
+// fs.rename("abcd.txt", "hey.txt", function (err) {
+//     if (err) console.log(err)
+//     else console.log("done")
+// })
+
+//? for delete file
+fs.unlink("hey.txt", function(err){
+    if (err) console.log(err)
+        else console.log("deleted")
+})
