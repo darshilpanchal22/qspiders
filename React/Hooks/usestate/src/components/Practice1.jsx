e71039aff91dbf20fafd0d3f52a8ff1aae21d72a
darshilpanchal22 <panchaldarshil145@gmail.com>
2026-01-17 13:18:34 +0530

    .

--- React/Hooks/usestate/src/components/Practice1.jsx
+++ React/Hooks/usestate/src/components/Practice1.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Practice1 = () => {
+    const [count, setcount] = useState(0)
     return (
         <div className='counter'>
-            <p id='para'>you have clicked 0 times</p>
-            <button id='btn'>click me</button>
+            <p id='para'>you have clicked {count} times</p>
+            <button id='btn' onClick={() => { setcount(count + 1) }}>click me</button>
         </div>
     )
 }
