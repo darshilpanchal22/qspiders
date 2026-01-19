18e2a351e01ecf8ac10a06e57acdb919118eeabd
darshilpanchal22 <panchaldarshil145@gmail.com>
2026-01-19 15:03:12 +0530

    .

---
+++ React/Event handeling/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const App = () => {
+  function handleclick() {
+    alert("clicked")
+  }
+  function handlemouseover() {
+    alert("para k par mouse")
+  }
+  function handleinputchange(e) {
+    // console.log("input is changed")
+    console.log("value till now ", e.target.value)
+
+  }
+  function handlesubmit(e) {
+    e.preventDefault();
+    //custom behaviout here
+    alert("form submit krdu kya")
+  }
+  return (
+
+    <div>
+      <button onClick={alert("")}></button>
+      {/* <form action="" onSubmit={handlesubmit}>
+        <input type="text" onChange={handleinputchange} />
+        <button type='submit'>submit</button>
+      </form>
+       <p onMouseOver={handlemouseover}>i ama para</p>
+       <button onClick={handleclick}>click me </button> */}
+    </div>
+  )
+}
+
+export default App
