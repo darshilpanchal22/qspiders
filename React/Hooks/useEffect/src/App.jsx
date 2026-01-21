55da5c1fe45f38a4ccb4191179fb562769ebeb3f
darshilpanchal22 <panchaldarshil145@gmail.com>
2026-01-21 15:00:37 +0530

    .

--- React/Hooks/useEffect/src/App.jsx
+++ React/Hooks/useEffect/src/App.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react'
+
 const App = () => {
+  const [count, setCount] = useState(0)
+  const [total, setTotal] = useState(1)
+  //  first = side effect function
+  // second  = clean-up  function 
+  // third = comma separeted dep list
+
+
+  //variation 1  = runs on every render
+  // useEffect(() => {
+  //   alert("helo")
+  // })
+
+  //variation 2 that runs on only first render
+  // useEffect(() => {
+  //   alert(" runs only 1 render")
+  // }, [])
+
+  //variation 3 
+  // useEffect(() => {
+  //   alert("i will run every timw when count is updated")
+  // }, [count])
+
+  //variation 4 
+  // useEffect(() => {
+  // alert("i will run only on first render (variation 4)")
+  // }, [])
+
+  //variation 5 = lets add cleanup function
+  useEffect(() => {
+    alert("count updated")
+    return () => {
+      alert("cleanup function called")
+    }
+  }, [count])
+
+  function handleClick() {
+    setCount(count + 1)
+    setTotal(total + 1)
+  }
+
+  function handleTotalClick() {
+    setTotal(total + 1)
+  }
+
   return (
     <div>
 
+      <button onClick={handleClick}>Increase Count & Total</button>
+      <br /><br />
+
+      <button onClick={handleTotalClick}>Increase Total Only</button>
+      <br /><br />
+
+      <p>Count: {count}</p>
+      <p>Total: {total}</p>
     </div>
   )
 }
