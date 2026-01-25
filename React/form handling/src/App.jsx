a8528de5a0283328289d3089ed7ca7c3c8c46fb5
darshilpanchal22 <panchaldarshil145@gmail.com>
2026-01-25 14:29:00 +0530

    .

--- React/form handling/src/App.jsx
+++ React/form handling/src/App.jsx
@@ -5,17 +5,31 @@ import React from 'react'
 const App = () => {
   const {
     register,
-
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm()
   return (
-    <form action="">
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
       <div>
         <label >
           First name
         </label>
+        <input {...register("fisrstname")} />
+      </div> 
+
+      <div>
+        <label >
+          Middle  name
+        </label>
+        <input {...register("middlename")} />
+      </div>
+
+      <div>
+        <label >
+          Last name
+        </label>
+        <input  {...register("lastname")} />
       </div>
     </form>
   )
