f299ebb6a8a2f66e07da57de2cd50711d399bd67
darshilpanchal22 <panchaldarshil145@gmail.com>
2026-02-02 12:37:48 +0530

    .

--- React/Hooks/useCallback/src/components/Child.jsx
+++ React/Hooks/useCallback/src/components/Child.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Child = React.memo(
     (props) => {
         console.log("re-render")
-        return (
+        return ( 
             <div>
                 <button onClick={props.handleclick}>
                     {props.buttonName}
