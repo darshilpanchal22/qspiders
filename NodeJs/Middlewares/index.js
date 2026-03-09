c7ffa8afbe114b4348c0c0d3e2b65873bfca3de6
darshilpanchal22 <panchaldarshil145@gmail.com>
2026-03-09 10:58:21 +0530

    .

--- NodeJs/Middlewares/index.js
+++ NodeJs/Middlewares/index.js
@@ -1,33 +1,31 @@
+//&loading middleware into the app
+//& inbuilt middleware
 const express = require('express')
 const app = express()
 const port = 3000
 
-//&loading middleware into the app
-//& inbuilt middleware
-
 app.use(express.json())
 
-//! middleware - logging auth validation
-
-const login = function (reg, res, next) {
+const login = function (req, res, next) {
     console.log("login kr raha hu")
     next()
 }
 app.use(login)
 
-const auth = function (reg, res, next) {
-    console.log("auth kr raha hu ")
+const auth = function (req, res, next) {
+    console.log("auth kr raha hu")
     next()
 }
 app.use(auth)
 
-const valid = function (re1, res, next) {
+const valid = function (req, res, next) {
     console.log("valid kr raha hu")
     next()
 }
 app.use(valid)
 
 app.get('/', (req, res) => {
+    console.log("route handler")
     console.log(req.body)
     res.send('Hello World!')
 })
